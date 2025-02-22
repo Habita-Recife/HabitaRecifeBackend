1e76cea65cc57e9670471c032e26055768d84419
gabriellamarinho <gabriellamarilia30@gmail.com>
2025-02-22 11:19:21 -0300

    arquivos iniciais

---
+++ habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/HabitaRecifeBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.habita_recife.habita_recife_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitaRecifeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitaRecifeBackendApplication.class, args);
+	}
+
+}
