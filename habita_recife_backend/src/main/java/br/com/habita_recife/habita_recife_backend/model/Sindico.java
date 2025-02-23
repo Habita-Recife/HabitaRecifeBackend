8d5892c0e36960f2525b18200bd8264a32621f93
gabriellamarinho <gabriellamarilia30@gmail.com>
2025-02-22 23:42:37 -0300

    update

--- habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/model/Sindico.java
+++ habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/model/Sindico.java
@@ -1,12 +1,21 @@
 package br.com.habita_recife.habita_recife_backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_sindico")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Sindico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +27,9 @@ public class Sindico {
     @Column(nullable = false, length = 15)
     private String telefone_sindico;
 
+    @Column(nullable = false, length = 10, unique = true)
+    private String rg_sindico;
+    
     @OneToMany(mappedBy = "sindico", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Relatorio> relatorios = new HashSet<>();
 
