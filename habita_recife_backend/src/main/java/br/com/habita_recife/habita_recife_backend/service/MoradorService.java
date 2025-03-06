1723d862af83ff4d7510da234663849138bf7e7e
norwalneto <norwalgomess@gmail.com>
2025-03-06 12:39:10 -0300

    implementado serviço e repository morador

---
+++ habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/service/MoradorService.java
@@ -0,0 +1,15 @@
+package br.com.habita_recife.habita_recife_backend.service;
+
+import br.com.habita_recife.habita_recife_backend.domain.model.Morador;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MoradorService {
+    List<Morador> listarTodos();
+    Optional<Morador> buscarPorId(Long id);
+    Morador salvar(MoradorDTO moradorDTO);
+    Morador atualizar(Long id, MoradorDTO moradorDTO);
+    void excluir(Long id);
+
+}
