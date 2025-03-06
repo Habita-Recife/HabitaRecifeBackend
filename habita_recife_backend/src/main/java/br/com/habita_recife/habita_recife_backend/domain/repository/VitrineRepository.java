4a27fe01336aabbb0a7d333378bfba688fb4c83d
gabriellamarinho <gabriellamarilia30@gmail.com>
2025-03-06 16:01:08 -0300

    update vitrine service

--- habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/domain/repository/VitrineRepository.java
+++ habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/domain/repository/VitrineRepository.java
@@ -1,9 +1,4 @@
 package br.com.habita_recife.habita_recife_backend.domain.repository;
 
-import br.com.habita_recife.habita_recife_backend.domain.model.Vitrine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public class VitrineRepository extends JpaRepository<Vitrine ,Long> {
-
+public interface VitrineRepository {
 }
