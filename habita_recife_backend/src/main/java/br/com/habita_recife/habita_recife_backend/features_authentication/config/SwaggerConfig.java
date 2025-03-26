ffefefc4949a1e64dbe09e994a4421e52be04d19
kevinalc1 <kevinalc@outlook.com>
2025-03-26 15:31:13 -0300

    update swagger

---
+++ habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/features_authentication/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package br.com.habita_recife.habita_recife_backend.features_authentication.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Habita Recife API")
+                        .version("1.0")
+                        .description("Documentação da API do sistema de gestão condominial."))
+                .addTagsItem(new Tag().name("Usuários").description("Gerenciamento de usuários"))
+                .addTagsItem(new Tag().name("Condomínios").description("Gestão de condomínios"))
+                .addTagsItem(new Tag().name("Autenticação").description("Endpoints de login e autenticação"));
+    }
+}
--- habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/features_authentication/service/UserService.java
+++ habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/features_authentication/service/UserService.java
@@ -4,8 +4,12 @@ package br.com.habita_recife.habita_recife_backend.features_authentication.servi
 import br.com.habita_recife.habita_recife_backend.features_authentication.dto.UserDTO;
 import br.com.habita_recife.habita_recife_backend.features_authentication.model.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User registerUser(UserDTO userDTO);
     User loginUser(UserDTO userDTO);
+
+    List<User> listarTodos();
 }
