aa1866e135f3d3b73994afb4ee6629053de09d49
gabriellamarinho <gabriellamarilia30@gmail.com>
2025-04-02 23:08:52 -0300

    alteração cors

--- habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/features_authentication/config/CorsConfig.java
+++ habita_recife_backend/src/main/java/br/com/habita_recife/habita_recife_backend/features_authentication/config/CorsConfig.java
@@ -14,8 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*");
+
             }
         };
     }
